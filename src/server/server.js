d2ed17444f2a13bd36bc0eeeb87d50e7c5906e98
Diego Luna <diegofcolunalopez@gmail.com>
2020-06-06 13:12:41 -0500

    Servir React con Express

--- src/server/server.js
+++ src/server/server.js
@@ -9,15 +9,15 @@ const { ENV, PORT } = process.env;
 
 const app = express();
 
-if (ENV === 'development') {
-  console.log('development config');
-  const webpackConfig = require('../../webpack.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
+if (ENV === "development") {
+  console.log("development config");
+  const webpackConfig = require("../../webpack.config");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
   const compiler = webpack(webpackConfig);
   const serverConfig = { port: PORT, hot: true };
-  
-  app.use(webpackDevMiddleware(compiler,serverConfig));
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
 }
 
@@ -25,7 +25,18 @@ if (ENV === 'development') {
 // respuest a nuestro usuario, res
 app.get("*", (req, res) => {
   // enviamos un
-  res.send({ hello: "express" });
+  res.send(`
+  <!DOCTYPE html>
+  <html>
+    <head>
+    <link rel="stylesheet" href="assets/app.css" type="text/css" >
+      <title>Platzi Video</title>
+    </head>
+    <body>
+      <div id="app"></div>
+      <script src="assets/app.js" type="text/javascript" ></script>
+    </body>
+  </html>`);
 });
 
 app.listen(PORT, (err) => {
