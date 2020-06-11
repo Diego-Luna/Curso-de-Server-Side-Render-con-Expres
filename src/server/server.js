8ed79616abc107a4439044cf50229e67f44d6b65
Diego Luna <diegofcolunalopez@gmail.com>
2020-06-11 15:55:08 -0500

    SSR

--- src/server/server.js
+++ src/server/server.js
@@ -43,7 +43,7 @@ const setResponse = (html, preloadedState, manifest) => {
   const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
   const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
-  
+
   return (`
   <!DOCTYPE html>
   <html>
