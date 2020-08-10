d1ae7aa59ae270eea1d260f0cafe5f2120e42cbb
Diego Luna <diegofcolunalopez@gmail.com>
2020-08-10 15:55:33 -0500

    Configuración de registro de usuario

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const setFavorite = payload => ({
   type: 'SET_FAVORITE',
   payload,
@@ -27,3 +29,26 @@ export const getVideoSource = payload => ({
   type: 'GET_VIDEO_SOURCE',
   payload,
 });
+
+export const setError = payload => ({
+  type: 'SET_ERROR',
+  payload,
+});
+
+export const registerUser = (payload, redirectUrl) => {
+  return (dispatch) => {
+    // hacemos la llamado con axios
+    axios.post('/auth/sign-up', payload)
+      .then(({ data }) => dispatch(registerRequest(data)))
+      .then(() => {
+        // si todo salio bien, reirecciona a la url que tenemos
+        window.location.href = redirectUrl;
+      })
+      .catch(
+        // si tenemos un error, lo tratamos
+        error => dispatch(setError(error))
+      )
+  };
+};
+
+export { setFavorite as default }
